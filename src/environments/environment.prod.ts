2bc73d8c23a04af30d68c81c0006d3d41fce0317
Juan Luis Guerrero Minero <jlguerrero@gmail.com>
2020-10-17 20:07:31 +0200

    * Environment config for firesbase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
   url: 'https://us-central1-angular-adv-goty.cloudfunctions.net',
   firebase: {
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
   }
 };
